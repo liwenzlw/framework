4ad6ebdfa484e49d03186d81bbdaf6d80a1ee93b
jimboi <jimboi@liwen>
2016-11-21 17:13:52 +0800

    init db

---
+++ framework/src/main/java/com/liwen/go/module/authz/helper/PasswordHelper.java
@@ -0,0 +1,43 @@
+package com.liwen.go.module.authz.helper;
+
+import org.apache.shiro.crypto.RandomNumberGenerator;
+import org.apache.shiro.crypto.SecureRandomNumberGenerator;
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.util.ByteSource;
+import org.springframework.stereotype.Component;
+
+import com.liwen.go.module.authz.bean.User;
+
+/**
+ * 重新计算保存到数据库中的密码
+ *
+ */
+@Component
+public class PasswordHelper {
+
+	private RandomNumberGenerator randomNumberGenerator = new SecureRandomNumberGenerator();
+	private String algorithmName = "md5";
+	private int hashIterations = 2;
+
+	public void setRandomNumberGenerator(RandomNumberGenerator randomNumberGenerator) {
+		this.randomNumberGenerator = randomNumberGenerator;
+	}
+
+	public void setAlgorithmName(String algorithmName) {
+		this.algorithmName = algorithmName;
+	}
+
+	public void setHashIterations(int hashIterations) {
+		this.hashIterations = hashIterations;
+	}
+
+	public void encryptPassword(User user) {
+
+		user.setSalt(randomNumberGenerator.nextBytes().toHex());
+
+		String newPassword = new SimpleHash(algorithmName, user.getPassword(),
+				ByteSource.Util.bytes(user.getCredentialsSalt()), hashIterations).toHex();
+
+		user.setPassword(newPassword);
+	}
+}
